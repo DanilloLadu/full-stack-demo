1b6474a1fccbc4b2ca00ffaa62200d356dd414de
Danillo <ladu.danillo@gmail.com>
2024-12-07 19:33:41 +0100

    Add jwt security on filter chain to check user and add jwt service to generate token

---
+++ backend/src/main/java/com/nillo/backend/role/Role.java
@@ -0,0 +1,38 @@
+package com.nillo.backend.role;
+
+import com.nillo.backend.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> user;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
