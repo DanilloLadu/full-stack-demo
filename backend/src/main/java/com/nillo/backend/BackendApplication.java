fdd3b216f90fcfe83d3e2fe3a9095ebb2089ffee
Danillo <ladu.danillo@gmail.com>
2024-12-07 20:01:53 +0100

    Add registration controller and service and email logic

--- backend/src/main/java/com/nillo/backend/BackendApplication.java
+++ backend/src/main/java/com/nillo/backend/BackendApplication.java
@@ -1,7 +1,14 @@
 package com.nillo.backend;
 
+import com.nillo.backend.role.Role;
+import com.nillo.backend.role.RoleRepository;
+import com.nillo.backend.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +17,17 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository, UserRepository userRepository) {
+		return args -> {
+
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(
+						Role.builder()
+								.name("USER")
+								.createdDate(LocalDateTime.now())
+								.build());
+			}
+		};
+	}
 }
