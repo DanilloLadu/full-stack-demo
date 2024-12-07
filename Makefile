5922f71de90d81c9b55b319becb8f8420d3575c1
Danillo <ladu.danillo@gmail.com>
2024-12-07 19:36:49 +0100

    Add OpenApiConfig to test the api and update Makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ open-browsers:
 	firefox "http://127.0.0.1:8080/api/v1/swagger-ui/index.html"
 	firefox "http://127.0.0.1:8025"
 
-start-dev:
-	./backend/mvnw install
-	./backend/mvnw spring-boot:run
+install:
+	./backend/mvnw -f backend/pom.xml clean install
+	make dev
+
+dev:
+	./backend/mvnw -f backend/pom.xml spring-boot:run
---
+++ backend/src/main/java/com/nillo/backend/config/OpenApiConfig.java
@@ -0,0 +1,50 @@
+package com.nillo.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Danillo",
+                        email = "danillo@test.be",
+                        url = "https://some-url.com"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - Alibou",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://127.0.0.1:8080/api/v1"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "Bearer Authentication"
+                )
+        }
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
